2b5536a1ca736290b070d05fea93a97685a51962
usman359 <m.usman095@gmail.com>
2024-12-13 22:47:47 +0500

    commited

--- app/api/instagram/auth/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/instagram/callback`;
-  const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
-  return NextResponse.redirect(authUrl);
-}
