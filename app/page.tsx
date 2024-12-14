375d59e15b8f8895712e1c805e22e21203ad6b21
usman359 <m.usman095@gmail.com>
2024-12-14 15:58:06 +0500

    commited

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,27 @@
 "use client";
 
-import React, { useEffect } from "react";
-import axios from "axios";
-import { useInstagram } from "./contexts/InstagramContext";
-import { useRouter } from "next/navigation";
+import LoginPage from "./login/page";
 
 export default function Page() {
-  const router = useRouter();
-  const { setPosts } = useInstagram();
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const response = await axios.get("/api/instagram_media");
-        console.log(response.data.data);
-        setPosts(response.data.data || []);
-      } catch (error) {
-        console.error("Error fetching Instagram media:", error);
-      }
-    }
+  // const router = useRouter();
+  // const { setPosts } = useInstagram();
+  // useEffect(() => {
+  //   async function fetchPosts() {
+  //     try {
+  //       const response = await axios.get("/api/instagram_media");
+  //       console.log(response.data.data);
+  //       setPosts(response.data.data || []);
+  //     } catch (error) {
+  //       console.error("Error fetching Instagram media:", error);
+  //     }
+  //   }
 
-    fetchPosts();
-  }, [setPosts]); // Add dependency array
+  //   fetchPosts();
+  // }, [setPosts]); // Add dependency array
 
   return (
     <div>
-      <button onClick={() => router.push("/posts")}>Go to posts</button>
+      <LoginPage />
     </div>
   );
 }
