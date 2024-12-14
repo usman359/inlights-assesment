395a389ac192e2554017b912ab0af2d600a52ab5
usman359 <m.usman095@gmail.com>
2024-12-14 16:02:37 +0500

    commited

--- app/login/page.js
+++ app/login/page.js
@@ -4,6 +4,7 @@ import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
   const handleLogin = () => {
+    console.log("Login with Instagram");
     signIn("instagram");
   };
 
